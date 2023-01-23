8ef896b5e84e60264665e4344bc48268e746afb6
“WenjieYang” <2785500y@student.gla.ac.uk>
2023-01-23 12:59:21 +0000

    Second comment

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rango import views
+from django.urls import include
 
 urlpatterns = [
+    path('rango/', include('rango.urls')),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
